4445e97b35a58bd8eabcfea6976498d31ab93dc6
Josef Ottosson <josef@local.josef.guru>
2019-05-14 22:36:32 +0200

    Initial commit

---
+++ ClassLibrary1/Configurator.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ClassLibrary1
+{
+    public static class Configurator
+    {
+        public static void AddApplication(this IServiceCollection services)
+        {
+            
+        }
+    }
+}
---
+++ MyApp/Startup.cs
@@ -0,0 +1,15 @@
+﻿using ClassLibrary1;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using MyApp;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace MyApp
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddApplication();
+        }
+    }
+}
